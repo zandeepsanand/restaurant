91702a6cc0cfac562f9d91d2d5fb6f13674e7451
zandeepsanand <sandeepsssanand@gmail.com>
2022-07-21 00:28:20 +0530

    without buid file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import HeaderRestaurant from './component/header/headerRestaurant';
+import CategoriesRestaurant from './component/categories/CategoriesRestaurant';
+import NavigationComponent from './component/navigation/navigationComponent';
+import SaladCard from './component/cards/salad/saladCard';
+import PizzaCard from './component/cards/pizza/pizzaCard';
+import PastaCard from './component/cards/pasta/pastaCard';
+import BurgerCard from './component/cards/burger/burgerCard';
+import BeveragesCard from './component/cards/beverages/beveragesCard';
+import DrinksCard from './component/cards/drinks/drinksCard';
+import ModalCard from './component/cards/modal/modalCard';
+import Footer from './component/footer/footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0">
+        <NavigationComponent/>
+      <HeaderRestaurant/>
+      
+      <CategoriesRestaurant/>
+      <SaladCard/>
+      <PizzaCard/>
+      <PastaCard/>
+      <BurgerCard/>
+      <BeveragesCard/>
+      <DrinksCard/>
+      <Footer/>
+      
+      
+      
+    </div>
     </div>
   );
 }
---
+++ src/component/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div className='footerblock'>
+            <div className='round-footer footer-color footer-s'>
+                <div className='content-footer'>My Order List / Next</div>
+            </div>
+        </div>
+    </div>
+  )
+}
